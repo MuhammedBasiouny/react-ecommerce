08cc9a2f42d33450f091459d1fdc2857fd9502a3
Mohammad Basiouny <muhammad.mouawad@gmail.com>
2022-06-21 15:45:58 +0200

    Home Category

--- src/Components/Home/HomeCategory.js
+++ src/Components/Home/HomeCategory.js
@@ -1,27 +1,36 @@
 import React from 'react';
-import { Container, Row } from 'react-bootstrap';
+import { Container, Row, Spinner } from 'react-bootstrap';
 import SubTitle from '../utils/SubTitle';
 import CategoryCard from './../category/CategoryCard';
-import cloth from '../../images/clothe.png';
-import cat2 from '../../images/cat2.png';
-import laptop from '../../images/labtop.png';
-import sale from '../../images/sale.png';
-import pic from '../../images/pic.png';
+import HomeCategoryHook from '../../hooks/category/home-category-hook';
+const HomeCategory = () => {
+  const [category, loading, colors] = HomeCategoryHook();
 
-function HomeCategory() {
   return (
     <Container>
       <SubTitle title='التصنيفات' btntitle='المزيد' pathText='/allcategory' />
       <Row className='my-2 d-flex justify-content-between'>
-        <CategoryCard title='اجهزة منزلية' img={cloth} background='#F4DBA4' />
-        <CategoryCard title='اجهزة منزلية' img={cat2} background='#F4DBA4' />
-        <CategoryCard title='اجهزة منزلية' img={laptop} background='#0034FF' />
-        <CategoryCard title='اجهزة منزلية' img={sale} background='#F4DBA4' />
-        <CategoryCard title='اجهزة منزلية' img={cloth} background='#FF6262' />
-        <CategoryCard title='اجهزة منزلية' img={pic} background='#F4DBA4' />
+        {loading === false ? (
+          category.data ? (
+            category.data.slice(0, 5).map((item, index) => {
+              return (
+                <CategoryCard
+                  key={index}
+                  title={item.name}
+                  img={item.image}
+                  background={colors[index]}
+                />
+              );
+            })
+          ) : (
+            <h4>لا يوجد تصنيفات</h4>
+          )
+        ) : (
+          <Spinner animation='border' variant='primary' />
+        )}
       </Row>
     </Container>
   );
-}
+};
 
 export default HomeCategory;
--- src/Components/category/CategoryContainer.js
+++ src/Components/category/CategoryContainer.js
@@ -1,38 +1,34 @@
 import React from 'react';
-import { Container, Row } from 'react-bootstrap';
-import cloth from '../../images/clothe.png';
-import cat2 from '../../images/cat2.png';
-import laptop from '../../images/labtop.png';
-import sale from '../../images/sale.png';
-import pic from '../../images/pic.png';
+import { Container, Row, Spinner } from 'react-bootstrap';
 import CategoryCard from './CategoryCard';
 
-function CategoryContainer() {
+const CategoryContainer = ({ data, loading }) => {
+  const colors = ['#FFD3E8', '#F4DBA5', '#55CFDF', '#FF6262', '#0034FF', '#FFD3E8'];
   return (
     <Container>
       <div className='admin-content-text mt-2 '>كل التصنيفات</div>
       <Row className='my-2 d-flex justify-content-between'>
-        <CategoryCard title='اجهزة منزلية' img={cloth} background='#F4DBA4' />
-        <CategoryCard title='اجهزة منزلية' img={cat2} background='#F4DBA4' />
-        <CategoryCard title='اجهزة منزلية' img={laptop} background='#0034FF' />
-        <CategoryCard title='اجهزة منزلية' img={sale} background='#F4DBA4' />
-        <CategoryCard title='اجهزة منزلية' img={cloth} background='#FF6262' />
-        <CategoryCard title='اجهزة منزلية' img={pic} background='#F4DBA4' />
-        <CategoryCard title='اجهزة منزلية' img={cloth} background='#F4DBA4' />
-        <CategoryCard title='اجهزة منزلية' img={cat2} background='#F4DBA4' />
-        <CategoryCard title='اجهزة منزلية' img={laptop} background='#0034FF' />
-        <CategoryCard title='اجهزة منزلية' img={sale} background='#F4DBA4' />
-        <CategoryCard title='اجهزة منزلية' img={cloth} background='#FF6262' />
-        <CategoryCard title='اجهزة منزلية' img={pic} background='#F4DBA4' />
-        <CategoryCard title='اجهزة منزلية' img={cloth} background='#F4DBA4' />
-        <CategoryCard title='اجهزة منزلية' img={cat2} background='#F4DBA4' />
-        <CategoryCard title='اجهزة منزلية' img={laptop} background='#0034FF' />
-        <CategoryCard title='اجهزة منزلية' img={sale} background='#F4DBA4' />
-        <CategoryCard title='اجهزة منزلية' img={cloth} background='#FF6262' />
-        <CategoryCard title='اجهزة منزلية' img={pic} background='#F4DBA4' />
+        {loading === false ? (
+          data ? (
+            data.map((item, index) => {
+              return (
+                <CategoryCard
+                  key={index}
+                  title={item.name}
+                  img={item.image}
+                  background={colors[Math.floor(Math.random() * 5) + 1]}
+                />
+              );
+            })
+          ) : (
+            <h4>لا يوجد تصنيفات</h4>
+          )
+        ) : (
+          <Spinner animation='border' variant='primary' />
+        )}
       </Row>
     </Container>
   );
-}
+};
 
 export default CategoryContainer;
